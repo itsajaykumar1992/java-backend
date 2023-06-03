3e58a76504a96d5406a87524964c2ac6a34f1643
Ajay <itsajaykumar1992@gmail.com>
2023-06-03 10:20:26 -0500

    Changes has been related to Class and Object Example

---
+++ com.examples/OOPS/Vehicle.java
@@ -0,0 +1,12 @@
+package com.examples.OOPS;
+
+public class Vehicle {
+
+    public void start() {
+        System.out.println("Vehicle Started..");
+    }
+
+    public void stop() {
+        System.out.println("Vehicle Stopped..");
+    }
+}
